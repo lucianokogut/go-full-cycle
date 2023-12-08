eb3abe257bdeaeefe3757e6ea7c86085d584039e
Luciano Kogut <ljkogut@hotmail.com>
2023-12-07 23:57:43 -0300

    wip: implementando os usecases de acordo com as chamadas dos métodos

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"encoding/json"
+
+	"br.com.lucianokogut/go-full-cycle-esquenta/internal/infra/akafka"
+	"br.com.lucianokogut/go-full-cycle-esquenta/internal/infra/repository"
+	"br.com.lucianokogut/go-full-cycle-esquenta/internal/usecase"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306/products")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	msgChan := make(chan *kafka.Message)
+	go akafka.Consume([]string{"products"}, "host.docker.internal:9094", msgChan)
+
+	repository := repository.NewProductRepositoryMysql(db)
+	createProductUsecase := usecase.NewCreateProductUseCase(repository)
+
+	for msg := range msgChan {
+		dto := usecase.CreateProductInputDto{}
+		err := json.Unmarshal(msg.Value, &dto)
+		if err != nil {
+			//TODO log do erro da aplicação
+		}
+		_, err = createProductUsecase.Execute(dto)
+	}
 }
