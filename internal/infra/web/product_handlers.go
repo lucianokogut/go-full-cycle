27706a278e5a2068bd315658ca4893a311e83c49
Luciano Kogut <ljkogut@hotmail.com>
2023-12-07 23:58:40 -0300

    wip: implementando as funções de acordo com as chamadas dos DTO's externos

---
+++ internal/infra/web/product_handlers.go
@@ -0,0 +1,37 @@
+package web
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"br.com.lucianokogut/go-full-cycle-esquenta/internal/usecase"
+)
+
+type ProductHandlers struct {
+	CreateProductUseCase *usecase.CreateProductUseCase
+	ListProductsUseCase  *usecase.ListProductsUseCase
+}
+
+func NewProductHandlers(createProductUseCase *usecase.CreateProductUseCase, listProductsUseCase *usecase.ListProductsUseCase) {
+	return &ProductHandlers{
+		CreateProductUseCase: createProductUseCase,
+		ListProductsUseCase:  listProductsUseCase,
+	}
+}
+
+func (p *ProductHandlers) CreateProductHandler(w http.ResponseWriter, r *http.Request) {
+	var input usecase.CreateProductInputDto
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	output, err := p.CreateProductUseCase.Execute(input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+}
